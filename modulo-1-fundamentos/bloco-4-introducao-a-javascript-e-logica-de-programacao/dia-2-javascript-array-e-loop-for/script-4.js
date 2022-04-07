3f892f07f7311623441b2fda05ff7cc3883eaaf2
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-04-07 19:14:46 -0300

    exercicios 3-9

---
+++ modulo-1-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script-4.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (number of numbers) {
+  soma += number;
+}
+
+valorFinal = soma / numbers.length;
+
+if (valorFinal > 20) {
+  console.log("valor maior que 20");
+} else {
+  ("valor menor ou igual a 20");
+}
---
+++ modulo-1-fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script-9.js
@@ -0,0 +1,9 @@
+let numbers = [];
+
+for (i = 1; i <= 25; i++) {
+  numbers.push(i);
+}
+
+for (i = 0; i < numbers.length; i++) {
+  console.log(numbers[i] / 2);
+}
