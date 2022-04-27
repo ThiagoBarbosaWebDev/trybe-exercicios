ba9dc6ae37c39b2a197843dc907faeb58695ef79
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-04-27 10:50:56 -0300

    bonus, exercicios feito com ajuda do gabarito

--- modulo-1-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ modulo-1-fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -2,13 +2,11 @@ window.onload = function () {
   const clearButton = document.querySelector("#clear");
   clearButton.addEventListener("click", clearForm);
   const button = document.querySelector("#submit-button");
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-  });
+  button.addEventListener("click", submitHandler);
   const agreement = document.querySelector("#agreement-checkbox");
   agreement.addEventListener("change", function () {
-    const submitButton = document.querySelector("#submit-button");
-    submitButton.disabled = !agreement.checked;
+  const submitButton = document.querySelector("#submit-button");
+  submitButton.disabled = !agreement.checked;
   });
 };
 
@@ -21,4 +19,33 @@ function clearForm() {
     field.checked = false;
   }
   textArea.value = "";
+}
+
+function validationHandler() {
+  const nomeCompleto = document.querySelector("#nome-id").value.length;
+  const nomeInvalido = nomeCompleto < 10 || nomeCompleto > 40;
+
+  const email = document.querySelector("#email-id").value.length;
+  const emailInvalido = email < 10 || email > 50;
+
+  const textArea = document.querySelector("textarea").value.length;
+  const textAreaInvalida = textArea > 500;
+
+  if (emailInvalido || nomeInvalido || textAreaInvalida) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function submitHandler(e) {
+  e.preventDefault();
+  const validation = validationHandler();
+  if (validation === false) {
+    alert("Dados inválidos");
+  } else {
+    alert(
+      "Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip."
+    );
+  }
 }
