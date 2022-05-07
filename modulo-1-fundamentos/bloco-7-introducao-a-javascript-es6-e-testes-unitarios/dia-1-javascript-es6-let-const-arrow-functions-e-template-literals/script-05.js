fb8f00e303652ad011e3b23bd04272eb11ff06bf
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-06 21:37:59 -0300

    exercicio 5

---
+++ modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script-05.js
@@ -0,0 +1,7 @@
+const array = ['HTML', 'javascript', 'css', 'react', 'redux'];
+
+let teste = "tryber x aqui"
+
+const replateLetter = (str, newStr) => str.replace('x',newStr)
+
+console.log(replateLetter(teste, 'bebeto'))
