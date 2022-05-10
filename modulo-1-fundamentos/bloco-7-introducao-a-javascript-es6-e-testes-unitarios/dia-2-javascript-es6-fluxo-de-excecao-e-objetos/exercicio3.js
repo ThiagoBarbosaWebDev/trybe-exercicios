61b42c4f2252405e594400a28cf02f924c8fca5e
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-09 22:06:40 -0300

    exercicio 5 parte 2

--- modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
+++ modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
@@ -33,8 +33,6 @@ console.log(showObjSize(lesson1))
 const showObjValues = obj => Object.values(obj)
 console.log(showObjValues(lesson1)) 
 
-// Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 
-// Crie um objeto de nome allLessons, que deve agrupar todas as aulas através do Object.assign. 
-// Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1, lesson2 e lesson3. 
-// Ao executar o comando console.log(allLessons), a saída deverá ser a seguinte:
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})  
+console.log(allLessons)
