da3a717e58f3cbcff7f439362ffd66bc973945fe
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-09 22:14:08 -0300

    exercicio 6 parte 2

--- modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
+++ modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
@@ -24,15 +24,23 @@ const addNewProperty = (targetObj, key, value) => {
 
 addNewProperty(lesson2, 'turno', 'noite')
 
-const showObjKeys = obj => Object.keys(obj)
-console.log(showObjKeys(lesson1))
+// const showObjKeys = obj => Object.keys(obj)
+// console.log(showObjKeys(lesson1))
 
-const showObjSize = obj => Object.keys(obj).length
-console.log(showObjSize(lesson1))
+// const showObjSize = obj => Object.keys(obj).length
+// console.log(showObjSize(lesson1))
 
-const showObjValues = obj => Object.values(obj)
-console.log(showObjValues(lesson1)) 
-
+// const showObjValues = obj => Object.values(obj)
+// console.log(showObjValues(lesson1)) 
 
 const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})  
-console.log(allLessons)
+
+const totalEstudantes = obj => {
+  let sum = 0;
+  for (i=0; i < Object.keys(obj).length; i += 1) {
+    sum += obj[`lesson${i + 1}`].numeroEstudantes;
+  }
+  return sum
+}
+
+console.log(totalEstudantes(allLessons))
