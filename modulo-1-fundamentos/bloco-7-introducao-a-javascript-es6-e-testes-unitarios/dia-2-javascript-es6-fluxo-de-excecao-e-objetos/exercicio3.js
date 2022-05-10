de629ab4686309fb3c91270fddecb270bcf036f9
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-10 10:34:44 -0300

    exercicio3.js

--- modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
+++ modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/exercicio3.js
@@ -1,67 +1,79 @@
 const lesson1 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 20,
-  professor: 'Maria Clara',
-  turno: 'manhã',
+  professor: "Maria Clara",
+  turno: "manhã",
 };
 
 const lesson2 = {
-  materia: 'História',
+  materia: "História",
   numeroEstudantes: 20,
-  professor: 'Carlos',
+  professor: "Carlos",
 };
 
 const lesson3 = {
-  materia: 'Matemática',
+  materia: "Matemática",
   numeroEstudantes: 10,
-  professor: 'Maria Clara',
-  turno: 'noite',
+  professor: "Maria Clara",
+  turno: "noite",
 };
 
 const addNewProperty = (targetObj, key, value) => {
-  targetObj[key] = value
-}
+  targetObj[key] = value;
+};
 
-addNewProperty(lesson2, 'turno', 'noite')
+addNewProperty(lesson2, "turno", "noite");
 
-const showObjKeys = obj => Object.keys(obj)
-console.log(showObjKeys(lesson1))
+const showObjKeys = (obj) => Object.keys(obj);
+console.log(showObjKeys(lesson1));
 
-const showObjSize = obj => Object.keys(obj).length
-console.log(showObjSize(lesson1))
+const showObjSize = (obj) => Object.keys(obj).length;
+console.log(showObjSize(lesson1));
 
-const showObjValues = obj => Object.values(obj)
-console.log(showObjValues(lesson1)) 
+const showObjValues = (obj) => Object.values(obj);
+console.log(showObjValues(lesson1));
 
-const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})  
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
 
-const totalEstudantes = obj => {
+const totalEstudantes = (obj) => {
   let sum = 0;
-  for (i=0; i < Object.keys(obj).length; i += 1) {
+  for (i = 0; i < Object.keys(obj).length; i += 1) {
     sum += obj[`lesson${i + 1}`].numeroEstudantes;
   }
-  return sum
-}
+  return sum;
+};
 
-console.log(totalEstudantes(allLessons))
+console.log(totalEstudantes(allLessons));
 
 const getValueByNumber = (obj, index) => {
-  const valueArray = Object.values(obj)[index]
-  console.log(valueArray)
-  
-}
+  const valueArray = Object.values(obj)[index];
+  console.log(valueArray);
+};
 
-getValueByNumber(lesson1, 0)
+getValueByNumber(lesson1, 0);
 
 const verifyPair = (obj, chave, valor) => {
   let array = [chave, valor];
   let objArray = Object.entries(obj);
-  let isEqual = false
-  objArray.forEach(subarray => {
-    if (subarray[0] === array[0] && subarray[1] === array[1]) {isEqual = true}
+  let isEqual = false;
+  objArray.forEach((subarray) => {
+    if (subarray[0] === array[0] && subarray[1] === array[1]) {
+      isEqual = true;
+    }
   });
-  return isEqual
-}
+  return isEqual;
+};
 
-console.log(verifyPair(lesson1, 'materia', 'Matemática'))
+console.log(verifyPair(lesson1, "materia", "Matemática"));
 
+const mathStudentTally = (obj, target) => {
+  let tally = 0;
+  for (let element of Object.entries(obj)) {
+    if (element[1].materia === target) {
+      tally += element[1].numeroEstudantes;
+    }
+  }
+  console.log(tally);
+};
+
+mathStudentTally(allLessons, "Matemática");
