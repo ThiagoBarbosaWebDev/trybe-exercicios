71ac464e56c0d706f170c88e9dda1808bbdd1814
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-10 20:39:37 -0300

    exercicio 1

--- modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script-01.test.js
+++ modulo-1-fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script-01.test.js
@@ -4,4 +4,13 @@ describe('Requisito 1', () =>{
   it('Testa de a soma é igual a 9', () => {
     expect(sum(4,5)).toBe(9)
   })
+  it('Testa de a soma é igual a 0', () => {
+    expect(sum(0,0)).toBe(0)
+  })
+  it('Teste se a função sum lança um erro quando os parâmetros são 4 e "5"(string 5)', () => {
+    expect(() => sum(4,'5')).toThrow()
+  })
+  it('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => sum(4,'5')).toThrow('parameters must be numbers')
+  })
 })
