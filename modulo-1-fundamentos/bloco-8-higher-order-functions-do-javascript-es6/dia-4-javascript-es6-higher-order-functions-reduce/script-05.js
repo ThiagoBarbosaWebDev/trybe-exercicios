e6225be21090c8aea0cc5076d61fe192c7c21cec
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-05-18 14:39:01 -0300

    refactor exercicio5

--- modulo-1-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script-05.js
+++ modulo-1-fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/script-05.js
@@ -8,11 +8,7 @@ const names = [
 function containsA() {
   // escreva seu código aqui
   let array = [];
-  names.forEach((str) => array.push(str.match(/a/gi).length))
-  return array.reduce((acc, num) => acc + num, 0)
-  // return names.reduce((acc, str) => acc.match(/a/gi).length + str.match(/a/gi).length, 0)
+  return names.reduce((acc, str) => acc + str.match(/a/gi).length, 0)
 }
 
 console.log(containsA()) 
-
-// console.log(names[0].match(/a/gi).length)
