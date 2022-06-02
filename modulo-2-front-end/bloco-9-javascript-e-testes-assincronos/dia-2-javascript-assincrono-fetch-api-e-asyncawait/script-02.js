246a7f1b1b70785411f55b49f2cb723fea55afdb
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-06-01 22:32:40 -0300

    refactor

--- modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
+++ modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
@@ -5,19 +5,20 @@ const btc = document.querySelector('#btc')
 
 const fetchCoins = async (crypto) => {
   const url = 'https://api.coincap.io/v2/assets';
-
+  
   return await fetch(url)
-    .then((response) => response.json())
-    .then((data) => data.data.filter(({rank}) => rank <= 10))
+  .then((response) => response.json())
+  .then((data) => data.data.filter(({rank}) => rank <= 10))
 }
 
-const append = (arr) => {
-  
+const append = async () => {
+  arr = await fetchCoins()
   arr.forEach(({id, priceUsd, symbol}) => {
     const newLi = document.createElement('li');
     newLi.innerText = `${id} (${symbol}): ${priceUsd}`
     btc.appendChild(newLi)
   });
 }
-// Bitcoin (BTC): 46785.06.
-const coinStonks = fetchCoins().then(append)
+
+append()
+`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json`
