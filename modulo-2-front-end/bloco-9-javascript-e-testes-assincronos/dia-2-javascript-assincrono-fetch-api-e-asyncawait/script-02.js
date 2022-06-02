be8b2cfac15e1f12a782930ff7e327d540000455
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-06-01 22:16:24 -0300

    exercicios 2-3-4

--- modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
+++ modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
@@ -1,14 +1,23 @@
 // api.js
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 
-const fetchCoins = async () => {
+const btc = document.querySelector('#btc')
+
+const fetchCoins = async (crypto) => {
   const url = 'https://api.coincap.io/v2/assets';
 
   return await fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data))
+    .then((data) => data.data.filter(({rank}) => rank <= 10))
 }
 
-
-
-fetchCoins();
+const append = (arr) => {
+  
+  arr.forEach(({id, priceUsd, symbol}) => {
+    const newLi = document.createElement('li');
+    newLi.innerText = `${id} (${symbol}): ${priceUsd}`
+    btc.appendChild(newLi)
+  });
+}
+// Bitcoin (BTC): 46785.06.
+const coinStonks = fetchCoins().then(append)
