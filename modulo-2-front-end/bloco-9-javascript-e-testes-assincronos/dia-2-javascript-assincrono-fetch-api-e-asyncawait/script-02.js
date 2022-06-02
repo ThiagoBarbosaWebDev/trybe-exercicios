b8c83108a404d1224ab26194f312588e5a2cd2c3
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-06-02 10:00:45 -0300

    bonus

--- modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
+++ modulo-2-front-end/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/script-02.js
@@ -6,19 +6,27 @@ const btc = document.querySelector('#btc')
 const fetchCoins = async (crypto) => {
   const url = 'https://api.coincap.io/v2/assets';
   
-  return await fetch(url)
+  return fetch(url)
   .then((response) => response.json())
   .then((data) => data.data.filter(({rank}) => rank <= 10))
 }
 
+const getBrlRates = async () => {
+  const ratesUrl = 'https://api.coincap.io/v2/rates';
+  return fetch(ratesUrl)
+  .then((response) => response.json())
+  .then ((data) => data.data.find(({symbol}) => symbol === 'BRL').rateUsd)
+}
+
 const append = async () => {
-  arr = await fetchCoins()
-  arr.forEach(({id, priceUsd, symbol}) => {
+  const crypto = await fetchCoins()
+  const rates = await getBrlRates()
+  crypto.forEach(({id, priceUsd, symbol}) => {
     const newLi = document.createElement('li');
-    newLi.innerText = `${id} (${symbol}): ${priceUsd}`
+    newLi.innerText = `${id} (${symbol}): R$ ${(priceUsd / rates).toFixed(2)}`
     btc.appendChild(newLi)
   });
 }
 
 append()
-`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json`
+
