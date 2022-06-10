efb3aace437f63ba6455827452ca181ce1dbfb4f
ThiagoBarbosaDev <thiagobarbosawebdev@gmail.com>
2022-06-10 15:16:44 -0300

    Exercicio 1 - Req2

--- modulo-2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-01/src/App.js
+++ modulo-2-front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-01/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+// const compromissos = ['Acordar', 'Codar', 'Dormir'];
+
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 class App extends React.Component {
   render() {
-
+    return Task(12);
   }
 }
  
